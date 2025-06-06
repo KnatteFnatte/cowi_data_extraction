a50ec07b7f4a480c5b9298038dfb13115e8ed603
KnatteFnatte <rpv922@alumni.ku.dk>
2025-06-06 19:01:07 +0200

    Update DataExtraction.py
    Completed the function for extracting forces and displacements
    
    Should still make it so that it creates two output files - one for raw data and one for averaged data

--- DataExtraction.py
+++ DataExtraction.py
@@ -4,6 +4,11 @@ import argparse
 import os
 import csv
 import shutil
+from io import StringIO
+
+
+
+
 
 class Datafile:
     def __init__(self, filename):
@@ -17,39 +22,82 @@ class Datafile:
         self.clay_type = name_parts[0]
         self.sand_content = name_parts[1]
         self.water_content = name_parts[2]
+        self.Co2 = False
+        self.recompression = False
+        self.new_wc = None
+        self.avg_max_displacement = None
+        self.avg_max_force = None
+        self.lime_content = None
+
         if "L" in name_parts[3]:
             self.lime_content = name_parts[3]
             del name_parts[3]
+
         if "Cu" in name_parts[3]:
             self.curing = True
         else:
             self.curing = False
+
         if "Dr" in name_parts[3]:
             self.drying = True
         else:
             self.drying = False
         self.temperature = name_parts[3][5:]
+
+        #Add here any optional flags in naming convention (landerslev for example has Calcium which Egernsund does not)
         if len(name_parts) > 4:
             if name_parts[4] == "Co2":
                 self.Co2 = True
                 del name_parts[4]
-            else:
-                self.Co2 = False
-        if len(name_parts) > 4:
+                
             if "Reco" in name_parts[4]:
                 self.recompression = True
                 self.new_wc = name_parts[4][6:]
-        else:
-            self.recompression = False
-            self.new_wc = None
+                del name_parts[4]
+            
+            if "Wet" in name_parts[4]:
+                self.testtype = "WetCompression"
+            
+
+        
+
         return
     def __str__(self):
         """Return a string representation of the Datafile object. It is named __str__ so that if the class instance is printed, the relevant data will be supplied"""
-        return f"Clay Type: {self.clay_type}, Sand Content: {self.sand_content}, Water Content: {self.water_content}, Lime Content: {self.lime_content}, Curing: {self.curing}, Drying: {self.drying}, Temperature: {self.temperature}, CO2: {self.Co2}, Recompression: {self.recompression}, New WC: {self.new_wc}"
+        return f"Clay Type: {self.clay_type}, Sand Content: {self.sand_content}, Water Content: {self.water_content}, Lime Content: {self.lime_content}, Curing: {self.curing}, Drying: {self.drying}, Temperature: {self.temperature}, CO2: {self.Co2}, Recompression: {self.recompression}, New WC: {self.new_wc}. avg_max_force: {self.avg_max_force}, avg_max_displacement: {self.avg_max_displacement}"
     
     def extract_data(self):
-        #pd.read_csv(self.filename, sep="\t", header=None, names=["Time", "Stress", "Strain", "Temperature"])
-        output = str(f"{self.clay_type}, {self.sand_content}, {self.water_content}, {self.lime_content}, {self.curing}, {self.drying}, {self.temperature}, {self.Co2}, {self.recompression}, {self.new_wc}")
+        """Extract the max force value and corresponding displacements from the CSV file. each file will have columnnumbers/3 values, as they should be concatenated laterally from the Instrom machine."""
+        if not os.path.isfile(self.filename):
+            raise ValueError(f"The file {self.filename} does not exist.")
+        # Read the CSV file using pandas
+        # We assume the CSV file has a header row, so we skip the first row
+        df = pd.read_csv(self.filename, sep=",", header=1)
+
+        # Create a list to hold the extracted data
+        extracted_data = []
+        for columns in df:
+            # Extract the relevant columns for this set of data
+
+            StringData = StringIO(df[columns].to_string(index = False, header = False))
+            # Read the CSV data from the string
+            new_df = pd.read_csv(StringData, header = 1, sep=";")
+            # Rename the columns for clarity
+            new_df.columns = ["Time (s)","Displacement (mm)", "Force (kN)"]
+            # Get the maximum force value and its corresponding displacement
+            max_force = new_df["Force (kN)"].max()
+            max_displacement = new_df.loc[new_df["Force (kN)"] == max_force, "Displacement (mm)"].values[0]
+            # Append the extracted data to the list
+            extracted_data.append((max_force, max_displacement))
+
+        # If we have multiple sets of data, we can average the max forces and displacements
+        self.max_forces = [data[0] for data in extracted_data]
+        self.max_displacements = [data[1] for data in extracted_data]
+        self.avg_max_force = np.mean(self.max_forces)
+        self.avg_max_displacement = np.mean(self.max_displacements)
+        self.force_std = np.std(self.max_forces)
+        # Return the values of the class as a string
+        output = str(f"{self.clay_type}, {self.sand_content}, {self.water_content}, {self.lime_content}, {self.curing}, {self.drying}, {self.temperature}, {self.Co2}, {self.recompression}, {self.new_wc}, {self.avg_max_force}, {self.force_std}")
         return output.split(", ")
 
 def get_filenames(directory):
@@ -57,6 +105,34 @@ def get_filenames(directory):
     if not os.path.isdir(directory):
         raise ValueError(f"The directory {directory} does not exist.")
     return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.csv')]
+
+#Function for collecting data outputs from single test (Instrom creates multiple CSV files - we only want one)
+
+def collect_csv(directory):
+    """Take a folder of csv files and concatenate them laterally (designed for csv files with 3 columns)"""
+    olddir = os.getcwd()
+    os.chdir(directory)
+    #First access all the filenames
+    if not os.path.isdir(directory):
+        raise ValueError(f"The directory {directory} does not exist.")
+    filearr = [f for f in os.listdir(directory) if (os.path.isfile(os.path.join(directory, f)) and f.endswith('.csv') or os.path.isfile(os.path.join(directory, f)) and f.endswith('.txt'))]
+    final_filename = filearr[0][:-4].split("_")
+    print(final_filename)
+    final_filename = [i for i in final_filename if not i.isdigit()]
+    final_filename = "_".join(final_filename)
+    final_filename = final_filename + ".csv"
+    #We want to concatenate all the data laterally, which is lengthy without pandas.
+    print(final_filename)
+    masterdf = pd.read_csv(filearr[0], delimiter=",")
+    for i in filearr[1:]:
+        tempdf = pd.read_csv(i, delimiter=",")
+        masterdf = pd.concat([masterdf, tempdf], axis=1)
+    
+    masterdf.to_csv(directory+"/"+final_filename, index=False, sep=",", header=True)
+    os.chdir(olddir)
+    print(os.getcwd())
+
+
 
 def merge_output_files(output_file, input_files):
     """Merge multiple output files into a single output file."""
@@ -77,9 +153,9 @@ def main():
     # Get the if the target directory contains any csv files
     file_list = get_filenames(os.getcwd())
     csv_file = csv.writer(open("output.csv", "w", newline=""))
-    csv_file.writerow("Clay type, Sand content, Water content, Lime content, Curing, Drying, Temperature, CO2, Recompression, New WC".split(", "))  
+    csv_file.writerow("Clay type, Sand content, Water content, Lime content, Curing, Drying, Temperature, CO2, Recompression, New WC, Mean_Max_Force, Standard Deviation".split(", "))  
 
-
+    """
     ############# TEST ###############
     datafile = Datafile("ERS_S15_W15.8_L5_Cu28-C40_Co2.csv")
     #print(datafile)
@@ -87,7 +163,7 @@ def main():
     csv_file.writerow(output)
     csv_file.writerow(output)  
     ############# END TEST ###############
-
+    """
 
     if file_list == []:
         print("No csv files found in the current working directory.")
@@ -102,7 +178,7 @@ def main():
         try:
             datafile = Datafile(file)
             #print(datafile)
-            output = file.extract_data()
+            output = datafile.extract_data()
             csv_file.writerow(output)
         except ValueError as e:
             print(f"Error processing file {file}: {e}")
