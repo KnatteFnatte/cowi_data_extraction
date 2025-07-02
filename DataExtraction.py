7a27811081212ce47696545344fb073701c56868
KnatteFnatte <peterrischel@gmail.com>
2025-07-02 19:44:04 +0200

    Dataplotting
    Added new notebook for data plotting and began work on function similar to extract_data which can plot data based on the files in a folder. Needs to add stress/strain option for function and pretty up the plots.

--- DataExtraction.py
+++ DataExtraction.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import csv
 import shutil
+import matplotlib.pyplot as plt
 from io import StringIO
 
 
@@ -109,12 +110,39 @@ class Datafile:
         # Return the values of the class as a string
         output = str(f"{self.clay_type}, {self.sand_content}, {self.water_content}, {self.lime_content}, {self.curing}, {self.drying}, {self.temperature}, {self.Co2}, {self.recompression}, {self.new_wc}, {self.avg_max_force}, {self.force_std}, {self.mean_pressure}, {self.pressure_std}")
         return output.split(", ")
+    
+    def plot_data(self):
+        """Plot the data from the CSV file."""
+        # Read the CSV file using pandas
+        df = pd.read_csv(self.filename, sep=",", header=1)
+        dfarray = []
+        # Create a list to hold the extracted data
+        for columns in df:
+            # Extract the relevant columns for this set of data
+            StringData = StringIO(df[columns].to_string(index = False, header = False))
+            # Read the CSV data from the string
+            new_df = pd.read_csv(StringData, header = 1, sep=";")
+            # Rename the columns for clarity
+            if len(new_df.columns) == 4:
+                new_df.columns = ["Time (s)", "Displacement (mm)", "Force (kN)", "Unused"]
+                
+            elif len(new_df.columns) == 3:
+                new_df.columns = ["Time (s)","Displacement (mm)", "Force (kN)"]
+            dfarray.append(new_df)
+            # Plot the data
+        return dfarray
 
 def get_filenames(directory):
     """Get all filenames in the specified directory."""
     if not os.path.isdir(directory):
         raise ValueError(f"The directory {directory} does not exist.")
-    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and f.endswith('.csv')]
+    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and (f.endswith('.csv') or f.endswith('.txt'))]
+
+def get_directories(directory):
+    """Get all directories in the specified directory. Use only in directory with data directories."""
+    if not os.path.isdir(directory):
+        raise ValueError(f"The directory {directory} does not exist.")
+    return [directory+"/"+d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
 
 #Function for collecting data outputs from single test (Instrom creates multiple CSV files - we only want one)
 
@@ -126,6 +154,7 @@ def collect_csv(directory):
     if not os.path.isdir(directory):
         raise ValueError(f"The directory {directory} does not exist.")
     filearr = [f for f in os.listdir(directory) if (os.path.isfile(os.path.join(directory, f)) and f.endswith('.csv') or os.path.isfile(os.path.join(directory, f)) and f.endswith('.txt'))]
+    #Remove file extension and give name to output file
     final_filename = filearr[0][:-4].split("_")
     print(final_filename)
     final_filename = [i for i in final_filename if not i.isdigit()]
@@ -138,8 +167,9 @@ def collect_csv(directory):
         tempdf = pd.read_csv(i, delimiter=",")
         masterdf = pd.concat([masterdf, tempdf], axis=1)
     
-    masterdf.to_csv(directory+"/"+final_filename, index=False, sep=",", header=True)
     os.chdir(olddir)
+    print(os.getcwd())
+    masterdf.to_csv(final_filename, index=False, sep=",", header=True)
     print(os.getcwd())
 
 
@@ -153,6 +183,47 @@ def merge_output_files(output_file, input_files):
     merged_df = pd.concat(dataframes, ignore_index=True)
     merged_df.to_csv(output_file, index=False, sep=",", header=True)
     return merged_df
+
+def main2(directory,subplotsax1, subplotsax2):
+    """This function is run manually through jupyter notebook to generate plots"""
+    os.chdir(directory)
+    cwd = directory
+    
+    # Get the if the target directory contains any csv files
+    file_list = get_filenames(cwd)
+
+    if file_list == []:
+        print("No csv files found in the current working directory.")
+        return
+    #Extract the data from the files
+    print(f"Found {len(file_list)} files in the target directory. Processing...")
+
+    fig, ax = plt.subplots(subplotsax1,subplotsax2, figsize=(20, 20))
+    ax = ax.flatten()
+    index = 0
+    for file in file_list:
+        if file == "output.csv":
+            print("WARNING: output.csv is in the target directory. The file will be overwritten.")
+            input("Press Enter to continue or Ctrl+C to exit.")
+            continue
+        try:
+            datafile = Datafile(file)
+            #print(datafile)
+            df = datafile.plot_data()
+            for i in df:
+                 ax[index].plot(i["Displacement (mm)"], i["Force (kN)"])
+            ax[index].set_xlabel("Displacement (mm)")
+            ax[index].set_ylabel("Force (kN)")
+
+        except ValueError as e:
+            print(f"Error processing file {file}: {e}")
+        index += 1
+
+    fig.tight_layout()
+    plt.show()
+    
+
+
 
 
 def main():
